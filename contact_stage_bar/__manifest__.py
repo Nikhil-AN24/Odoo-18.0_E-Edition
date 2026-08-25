332292fd8fb05ed529e1c44d20bce65b5e54a5ce
NikhiL. <nikhil.bansode@augmont.com>
2026-08-25 18:05:50 +0530

    manifest checking1

--- contact_stage_bar/__manifest__.py
+++ contact_stage_bar/__manifest__.py
@@ -1,6 +1,6 @@
 {
     "name": "Contacts Stage Bar",
-    "version": "18.0.1.7.75",
+    "version": "18.0.1.7.8",
     "sequence": -100,
     "category": "Contacts",
     "summary": "Add status bar stages to Contacts form and Kanban views",
